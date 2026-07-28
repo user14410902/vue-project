267d2df47323eb3db1cb8a598009b34df87c8716
your name <you@example.com>
2026-07-28 20:59:39 +0200

    Init commit for learning Composition API.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!-- https://vuejs.org/tutorial/#step-4-->
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue'
+
+
+const titleClass = ref('title')
+const message = ref('Hello World!')
+
+console.log(message.value) // "Hello World!"
+message.value = 'Changed'
+
+
+const counter = reactive({
+  count: 0
+})
+
+console.log(counter.count) // 0
+counter.count++
+
+</script>
+
+<template>
+  <h1>Make me dynamic!</h1>
+  <h1 :class="titleClass">{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
+
+<style scope>
+.title {
+  color: red;
+}
+</style>
